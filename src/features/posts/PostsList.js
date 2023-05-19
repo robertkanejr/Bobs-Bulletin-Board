d629bdd813e1fda9d7415c0b85220a6dd029e63d
Bob <bkane90@gmail.com>
2023-05-19 19:25:13 -0400

    build basic functionality

---
+++ src/features/posts/PostsList.js
@@ -0,0 +1,28 @@
+import { useSelector } from "react-redux";
+import { selectAllPosts } from "./postsSlice";
+import PostAuthor from "./PostAuthor";
+
+import React from "react";
+
+const PostsList = () => {
+	const posts = useSelector(selectAllPosts);
+
+	const renderedPosts = posts.map((post) => (
+		<article key={post.id}>
+			<h3>{post.title}</h3>
+			<p>{post.content.substring(0, 100)}</p>
+			<p className="postCredit">
+				<PostAuthor userId={post.userId} />
+			</p>
+		</article>
+	));
+
+	return (
+		<section>
+			<h2>Posts</h2>
+			{renderedPosts}
+		</section>
+	);
+};
+
+export default PostsList;
